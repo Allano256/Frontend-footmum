24c5c67b5b1b5b0c73dd86c4b5c41ff2107a41f1
allan zizinga <allanzizinga@yahoo.com>
2024-09-12 04:02:26 +0200

    Create the Routes

--- foot-mum/src/components/NavBar.jsx
+++ foot-mum/src/components/NavBar.jsx
@@ -5,19 +5,24 @@ import { IoHomeSharp } from "react-icons/io5";
 import { PiSignInBold } from "react-icons/pi";
 import { FaUserPlus } from "react-icons/fa6";
 import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom';
+
 
 const NavBar = () => {
   return (
     
       <Navbar className={styles.NavBar} expand="md" fixed='top' >
       <Container>
+        <NavLink to="/">
         <Navbar.Brand href="#home"> <img src={logo} alt='a logo' height="45px" ></img>   </Navbar.Brand>
+        </NavLink>
+        
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link > <IoHomeSharp/>  Home</Nav.Link>
-            <Nav.Link > <PiSignInBold/>Sign in</Nav.Link>
-            <Nav.Link > < FaUserPlus/>Sign up</Nav.Link>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/" exact > <IoHomeSharp/>  Home</NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin" > <PiSignInBold/>Sign in</NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="signup" > < FaUserPlus/>Sign up</NavLink>
            
 
           </Nav>
